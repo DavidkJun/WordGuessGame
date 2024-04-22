a88c39873a4419c00c83dc8707608fba6161c652
Davyd Krykota <krikotadavid957@gmail.com>
2024-04-22 18:46:15 +0300

    added function for filling up game area with cells

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
-const  gameArea = document.querySelector("#gamearea");
+const area = document.getElementById('area');
+const cell = document.getElementsByClassName('cell');
 
 const gamewidth = 5;
 const gameHeigth = 6;
+
+function createGameBoard() {
+    let cellIndex = 1;
+    for (let row = 0; row < 6; row++) {
+        for (let col = 0; col < 5; col++) {
+            area.innerHTML += "<div class='cell' pos=" + cellIndex + "></div>";
+            cellIndex++;
+        }
+    }
+}
+
+createGameBoard()
