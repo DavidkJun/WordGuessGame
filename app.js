f416444014b7bbb12b97113a3f9ac2e6ba785b04
Davyd Krykota <krikotadavid957@gmail.com>
2024-04-26 22:07:07 +0300

    improved naming

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const area = document.getElementById('area');
-const gameWidth = 5;
-const gameHeight = 6;
+const collums = 5;
+const rows = 6;
 
 const wordGuesses = {
     1: [],
@@ -13,8 +13,8 @@ const wordGuesses = {
 
 function createGameBoard() {
     let cellIndex = 1;
-    for (let row = 0; row < gameHeight; row++) {
-        for (let col = 0; col < gameWidth; col++) {
+    for (let row = 0; row < rows; row++) {
+        for (let col = 0; col < collums; col++) {
             area.innerHTML += "<div class='cell' id='cell_" + cellIndex + "' contenteditable='true'></div>";
             cellIndex++;
         }
@@ -28,7 +28,7 @@ const cells = document.getElementsByClassName('cell');
 for (const cell of cells) {
     cell.addEventListener("keyup", letterType);
 }
-
+console.log(wordGuesses)
 function letterType(event) {
     const key = event.key.toUpperCase();
     if (key.length === 1 && key.match(/[A-Z]/i)) { // Перевірка чи введено букву
